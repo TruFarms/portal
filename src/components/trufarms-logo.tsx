644e739d371694a378b431e337e0ad5193282b84
trufarms <trufarms351@gmail.com>
2025-09-06 22:24:22 +0000

    change logos to trufarms_trans2.png

--- src/components/trufarms-logo.tsx
+++ src/components/trufarms-logo.tsx
@@ -2,11 +2,10 @@ import type { ImgHTMLAttributes } from 'react';
 
 const TruFarmsLogo = (props: ImgHTMLAttributes<HTMLImageElement>) => (
 <img
-  src="/trufarms_trans.png"
+  src="/trufarms_trans2.png"
   alt="TruFarms Logo"
   className="w-[200px] h-auto" // Tailwind example
 />
 );
 
 export default TruFarmsLogo;
-
